325654380ba2f6a7d7d263e7819e4f68c96be00f
limongi1234 <93741519+limongi1234@users.noreply.github.com>
2022-09-20 12:19:12 -0300

    Update Temporada.java

--- model/Temporada.java
+++ model/Temporada.java
@@ -1,43 +1,35 @@
 package model;
 
-public class Temporada 
-{
+public class Temporada {
 	private int qtdEp, numTemporada;
 	
-	public Temporada(int qtdEp, int numTemporada) 
-	{
+	public Temporada(int qtdEp, int numTemporada) {
 		this.qtdEp = qtdEp;
 		this.numTemporada = numTemporada;
 	}
 
-	public Temporada()
-	{
+	public Temporada(){
 		super();
 	}
 
-	public int getQtdEp()
-	{				
+	public int getQtdEp(){				
 		return qtdEp;
 	}
 
-	public int getNumTemporada()
-	{
+	public int getNumTemporada(){
 		return numTemporada;
 	}
 
-	public void setQtdEp(int qtdEp) 
-	{
+	public void setQtdEp(int qtdEp) {
 		this.qtdEp = qtdEp;
 	}
 
-	public void setNumTemporada(int numTemporada)
-	{
+	public void setNumTemporada(int numTemporada){
 		this.numTemporada = numTemporada;
 	}
 
 	@Override
-	public String toString()
-	{
-		return "Temporadas: " + numTemporada + "\nQuantidade de episódios por temporada: " + qtdEp;
+	public String toString(){
+		return "Temporadas: " + numTemporada + "\nQuantidade de episÃ³dios por temporada: " + qtdEp;
 	}
 }
