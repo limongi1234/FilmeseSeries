002dcc31aee5fca6bbd653f7863b37df27ba5a58
limongi1234 <93741519+limongi1234@users.noreply.github.com>
2022-09-20 12:12:49 -0300

    Update Filme.java

--- model/Filme.java
+++ model/Filme.java
@@ -1,35 +1,29 @@
 package model;
 
-public final class Filme extends Programa 
-{
+public final class Filme extends Programa {
    private double duracao;
 
-	public double getDuracao() 
-	{
+	public double getDuracao() {
 		return duracao;
 	}
 
-	public Filme(String nome, ECategoria categoria, double duracao) 
-	{
+	public Filme(String nome, ECategoria categoria, double duracao) {
 	   super(nome, categoria);
 	
 	   this.duracao = duracao;
 	}
 	
-	public Filme()
-	{
+	public Filme(){
 		super();
 	}
 
-	public void setDuracao(double duracao)
-	{
+	public void setDuracao(double duracao){
 		this.duracao = duracao;
 	}
 
 	@Override
-	public String toString() 
-	{
-		return "Nome do programa: " + nome + "\nCategoria: " + categoria + "\nClassificação: " +pontuacao+"\nDuração do filme: "+duracao+" min\n"
+	public String toString() {
+		return "Nome do programa: " + nome + "\nCategoria: " + categoria + "\nClassificaÃ§Ã£o: " +pontuacao+"\nDuraÃ§Ã£o do filme: "+duracao+" min\n"
 				+ "---------------------\n";
 	}
 }
